a97a04dcac449638c31ff0150c8852bfcf4935a9
Михаил Негер <locky37@yandex.ru>
2024-12-05 21:47:20 +0300

    System.InvalidOperationException: 'The control ScrollViewer (Content = ListBox) already has a visual parent ContentPresenter (Name = PART_SelectedContentHost, Host = TabControl) while trying to add it as a child of ContentPresenter.'

---
+++ AvaloniaApplicationReact/Views/TabsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaApplicationReact.Views;
+
+public partial class TabsView : UserControl
+{
+    public TabsView()
+    {
+        InitializeComponent();
+    }
+}
