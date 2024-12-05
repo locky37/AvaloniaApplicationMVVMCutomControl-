f8ed5b7399ba95ab9b0a2af0d5d5e6ccda530e32
Михаил Негер <locky37@yandex.ru>
2024-12-05 22:28:50 +0300

    О да

--- AvaloniaApplicationReact/Views/MainView.axaml.cs
+++ AvaloniaApplicationReact/Views/MainView.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using AvaloniaApplicationReact.ViewModels;
 
 namespace AvaloniaApplicationReact.Views;
 
@@ -7,5 +8,6 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+        DataContext = new MainViewModel(); // Устанавливаем DataContext
     }
 }
